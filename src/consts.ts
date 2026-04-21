39a77f1862c24c8cad86da09f2baf247dba9739f
bastien-germain <bastien@regleselementaires.com>
2026-04-21 15:03:07 +0200

    informer

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Règles Élémentaires';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Règles Élémentaires agit pour l’égalité et la justice menstruelle. Depuis 2015, nous faisons des règles un enjeu de société pour qu’elles ne soient plus jamais un obstacle à la dignité, à la santé et à l’égalité.';
